b91bbdc21bfb63a42fa4a0fe6052d7643e0350f2
Akpams <81030047+Akpams@users.noreply.github.com>
2023-07-07 09:17:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def stream():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port="3000", debug=True)
+    app.run(host='0.0.0.0',port="8080", debug=True)
